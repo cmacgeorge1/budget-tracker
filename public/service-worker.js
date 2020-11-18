030725892d7fc90f39af599793665ac762a64116
Chris MacGeorge <christopher.s.macgeorge@gmail.com>
2020-11-17 23:36:56 -0800

    updated serviceworker

--- public/service-worker.js
+++ public/service-worker.js
@@ -31,7 +31,22 @@ self.addEventListener("install", function(evt) {
 
 
 // Activate
-
+self.addEventListener("activate", function(evt) {
+    evt.waitUntil(
+        caches.keys().then(keyList => {
+            return Promise.all(
+                keyList.map(key => {
+                    if (key!== CACHE_NAME && key !== DATA_CACHE_NAME) {
+                        console.log("Removing old cache data", key);
+                        return casches.delete(key);
+                    }
+                })
+            );
+        })
+    );
+    
+    self.clients.claim();
+});
 
 
 // Fetch
